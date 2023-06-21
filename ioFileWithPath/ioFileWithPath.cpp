fdd3cb68a67edefce7ceea2f2541a143e28e8af4
Lardzzz <aku.ikan04@gmail.com>
2023-06-21 15:33:27 +0700

    membuat fungsi untuk membaca variabel namafile

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -14,3 +14,22 @@ int main() {
 	ofstream outfile;
 	//menunjuk ke sebuah nama file
 	outfile.open(NamaFile + ".txt", ios::out);
+	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+	//unlimited loop untuk menulis
+	while (true) {
+		cout << "- ";
+		//mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		//loop akan berhenti jika anda memasukkan karakter q
+		if (baris == "q") break;
+		//menulis dan memasukkan nilai dari 'baris' ke dalam file
+		outfile << baris << endl;
+	}
+	//menutup file
+	outfile.close();
+	//membuka file dalam mode membaca
+	ifstream infile;
+	//menunjuk ke sebuah file
+	infile.open(NamaFile + ".txt", ios::in);
+
