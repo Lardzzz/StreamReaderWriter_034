a87f851b6ba6a19a828f659681366bb99b2dd04f
Lardzzz <aku.ikan04@gmail.com>
2023-06-21 15:57:27 +0700

    membuat catch default

--- manualException/manualException.cpp
+++ manualException/manualException.cpp
@@ -11,3 +11,9 @@ int main() {
 		//blok ini akan dieksekusi
 		cout << "pengecualian akan dieksekusi" << endl;
 	}
+	catch (...) {
+		//jika selain integer maka block ini akan dieksekusi
+		cout << "default pengecualian dieksekusi" << endl;
+	}
+	return 0;
+}
